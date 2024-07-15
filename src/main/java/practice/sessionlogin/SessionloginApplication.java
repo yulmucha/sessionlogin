d31625ae9c46346845f5eaaf5004b9793b2c38b5
Yul Jeon <dalbitstream@gmail.com>
2024-07-15 19:52:58 +0900

    Initial commit

---
+++ src/main/java/practice/sessionlogin/SessionloginApplication.java
@@ -0,0 +1,13 @@
+package practice.sessionlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/sessionlogin/SessionloginApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.sessionlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
